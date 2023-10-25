ff6070a03c0273dfb0fbc7a4e15ecaeef815a54f
Betzy V. Rosiles Moreno <brosilesmoreno@bsd220.org>
2023-10-25 09:46:28 -0500

    Initial Commit

---
+++ Roll the Dice Hackwitch/Roll_the_Dice_HackwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_Dice_HackwitchApp.swift
+//  Roll the Dice Hackwitch
+//
+//  Created by Betzy Moreno on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_Dice_HackwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
